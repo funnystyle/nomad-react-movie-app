db0bba2b3ca7186c856ffd18a6745c24396ddf9b
Funnystyle <esero@hanmail.net>
2021-04-20 17:17:48 +0900

    apply BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
@@ -8,12 +8,12 @@ import "./App.css";
 
 function App() {
   return (
-    <HashRouter>
+    <Router basename={"/nomad-react-movie-app"}>
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
       <Route path="/movie/:id" component={Detail} />
-    </HashRouter>
+    </Router>
   );
 }
 
